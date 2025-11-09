948a279b119282439f9b731d25a0dab19db2a22a
ABDE-LKADER <abadouab@student.1337.ma>
2025-11-09 21:51:05 +0100

    Mariadb Service:
    - Fix the problem of user not created with the `mariadbd --bootstrap`.
    - Replace `mariadbd` with the `init_file=` in mariadb config.

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
+DOCKER_COMPOSE_FILE = srcs/docker-compose.yml
+
+all: up
+
+up:
+	docker compose -f $(DOCKER_COMPOSE_FILE) up -d
+
+down:
+	docker compose -f $(DOCKER_COMPOSE_FILE) down
 
 clean:
-	docker system prune -af
+	docker system prune --volumes -af
+
+fclean: clean
+	rm -fr /Users/${USER}/data/mariadb
+
+logs:
+	docker compose -f $(DOCKER_COMPOSE_FILE) logs
+
+re: down fclean up
+
+.PHONY: up down clean
--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-exec mariadbd --user=mysql --bind-address=0.0.0.0 --port=3306
+# think about the restart for not init each time ...
+if [ ! -d /var/lib/mysql/mysql ]; then
+mariadb-install-db --user=mysql --skip-test-db
+cat << EOF > /var/lib/mysql/init.sql
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    FLUSH PRIVILEGES;
+EOF
+fi
+
+exec mariadbd --user=mysql
